5be75b860cb129e506b9ce0dce23bf235973db44
Kirill Larionov <kirill.kirill2@yandex.ru>
2024-12-25 23:06:37 +0500

    feat(schedule): page add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SchedulePageComponent } from './pages/schedule-page/schedule-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: '', component: HomePageComponent }],
+    children: [
+      { path: '', component: HomePageComponent },
+      {
+        path: 'schedule',
+        loadComponent: () => SchedulePageComponent,
+      },
+    ],
   },
 ];
---
+++ src/app/pages/schedule-page/schedule-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-schedule-page',
+  imports: [NgStyle],
+  templateUrl: './schedule-page.component.html',
+  styleUrl: './schedule-page.component.scss',
+})
+export class SchedulePageComponent {}
